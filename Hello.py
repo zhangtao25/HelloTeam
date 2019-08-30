560288f08211fb08211e033a36c5e234eb237baa
dai3023273 <963826680@qq.com>
2019-08-30 10:57:21 +0800

    合并hello.py

--- Hello.py
+++ Hello.py
@@ -4,7 +4,13 @@ for i in  range(5):
     print('*%d'%s)
 print("增加python语言的语言热度")
 
-print=("python是最好的语言")
-print=("python能跨平台")
+
+print("python是最好的语言")
+print("python能跨平台")
 print("提升自己才有选择的权利")
 print("否则，人方为刀俎，你为鱼肉")
+
+print("python是最好的语言")
+print("python能跨平台")
+print('身体坐直，保持程序员最帅的姿势')
+
